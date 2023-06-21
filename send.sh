ac36e216558496a6c78c2a13ad68b1dce693873f
Jayan Ratna <jayanratna@gmail.com>
2023-06-21 16:52:08 +1200

    Update `from` and `to` parameters

--- send.sh
+++ send.sh
@@ -2,8 +2,8 @@ curl -X POST 'https://api.resend.com/emails' \
   -H 'Authorization: Bearer re_123456789' \
   -H 'Content-Type: application/json' \
   -d $'{
-    "from": "onboarding@resend.dev",
-    "to": "delivered@resend.dev",
+    "from": "Acme <onboarding@resend.dev>",
+    "to": ["delivered@resend.dev"],
     "subject": "Hello World",
     "html": "<strong>it works!</strong>"
   }'
